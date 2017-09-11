2c96dd6b3ab42b75b84a8399509b3e0481261c1c
Taylor Bell <taylorbell@gmail.com>
2017-09-11 11:12:19 -0600

    remove all css, add tachyons

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,31 +2,13 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
-
-import './index.css'
+require('tachyons')
 
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
+  <div>
+    <div>
+      <h1>
+        <Link to="/">
           Gatsby
         </Link>
       </h1>
@@ -44,14 +26,7 @@ const TemplateWrapper = ({ children }) => (
       ]}
     />
     <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <div>
       {children()}
     </div>
   </div>
