bc2a283e0fda4579efb001725c9e6483be702318
Judge Maygarden <jmaygarden@gmail.com>
2017-11-29 18:46:22 -0600

    Added GitHub Pages deployment.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div className="center measure">
-    <h2>I'm Judge Maygarden.</h2>
     <p>I've spent my career prioritizing domain knowledge. Truly great software is built by those that fully understand the problem. The goal is not just correct software, but to build the right thing.</p>
     <p>With 15 years of experience in hardware/software development, I help clients make sure that they are building the right thing using the right tools. That could range from C firmware for a battery-powered device to Python applications crunching big data on a cloud cluster and everything in between.</p>
     <p>In addition to software architecture and development, I have years of experience running large projects from start to finish in regulated fields like medical devices and defense applications. I know how to properly scope, organize and validate development at scale.</p>
